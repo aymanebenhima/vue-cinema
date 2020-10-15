15ad8a45ed6ec7030fa42fc2f72237704cd84d5c
Aymane Benhima <aymanebenhima@gmail.com>
2020-10-15 01:16:09 +0100

    devided components

--- src/main.js
+++ src/main.js
@@ -1,51 +1,29 @@
 import Vue from 'vue';
-import './style.scss'
+import './style.scss';
 
-import genres from './util/genres';
+import MovieList from './components/MovieList.vue';
+import MovieFilter from './components/MovieFilter.vue';
 
 new Vue({
     el: '#app',
+    data: {
+        genre: [],
+        time: []
+    },
+    methods: {
+        checkFilter(category, title, checked) {
+            if (checked) {
+                this[category].push(title);
+            } else {
+                let index = this[category].indexOf(title);
+                if (index > -1) {
+                    this[category].splice(index, 1);
+                }
+            }
+        }
+    },
     components: {
-      'movie-list': {
-        template: `<div id="movie-list">
-                      <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
-                  </div>`,
-        data() {
-          return {
-            movies: [
-              { title: 'Pulp Fiction' },
-              { title: 'Home Alone' },
-              { title: 'Austin Powers' }
-            ]
-          }
-        }
-      },
-      'movie-filter': {
-        data() {
-          return {
-            genres
-          };
-        },
-        template: `<div id="movie-filter">
-                      <h2>Filter results</h2>
-                      <div class="filter-group">
-                        <check-filter v-for="genre in genres" :title="genre"></check-filter>                   
-                      </div>                 
-                  </div>`,
-        components: {
-          'check-filter': {
-                data() {
-                    return {
-                        checked: false
-                    }
-                },
-                props: ['title'],
-                template:   `<div :class="{ 'check-filter': true, active: checked }" @click="checked = !checked">
-                                <span class="checkbox"></span>
-                                <span class="check-filter-title">{{ title }}</span>
-                            </div>`
-          }
-        }
-      }
+        MovieList,
+        MovieFilter
     }
-  });
+});
