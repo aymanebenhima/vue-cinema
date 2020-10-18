d5ba33e189d6d598323dccdd097fdb5667e3b276
Aymane Benhima <aymanebenhima@gmail.com>
2020-10-18 01:19:24 +0100

    edited event bus

--- src/util/bus.js
+++ src/util/bus.js
@@ -1,12 +1,16 @@
- function checkFilter (category, title, checked) {
+  function checkFilter(category, title, checked) {
     if (checked) {
-        this[category].push(title);
+      this[category].push(title);
     } else {
-        let index = this[category].indexOf(title);
-        if (index > -1) {
-            this[category].splice(index, 1);
-        }
+      let index = this[category].indexOf(title);
+      if (index > -1) {
+        this[category].splice(index, 1);
+      }
     }
-}
-
-export { checkFilter }
+  }
+  
+  function setDay(day) {
+    this.day = day;
+  }
+  
+  export { checkFilter, setDay };
